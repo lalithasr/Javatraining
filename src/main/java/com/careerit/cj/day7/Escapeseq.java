445ff66d48ccd81fca4640dbfad1cf5197b052ed
lalithasr <lalithabadambvsr@gmail.com>
2024-09-25 12:31:16 -0400

    Adding the day 7 package which has class on Escape Sequence and Special characters

---
+++ src/main/java/com/careerit/cj/day7/Escapeseq.java
@@ -0,0 +1,15 @@
+package com.careerit.cj.day7;
+
+public class Escapeseq {
+
+    public static void main(String[] args) {
+        String text1 = "Hello\n How are you?";
+        String text2 = "Please find the file at location\r C:\\newfolder\\newfile.txt";
+        String text3 = "This is\tJava\tclass";
+        String text4 = "This is a\bJava class";
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+        System.out.println(text4);
+    }
+}
